2734d4940abb8e4ff832b6a57f60d5b083da9907
Nulljy <74478749+Nulljy@users.noreply.github.com>
2023-06-01 21:49:09 +0900

    Update app.js

--- MineSearch/app.js
+++ MineSearch/app.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {createContext, useReducer, useMemo} from 'react';
 import Form from './Components/Form';
+import Table from './Components/Table';
 
 
 export const CODE = {
@@ -19,6 +20,8 @@ export const MyContext = createContext({
     dispatch: () => {},
 });
 export const START_GAME = 'START_GAME';
+export const OPEN_BOX = 'OPEN_BOX';
+
 const initialState = {
     tableData: [],
     data: {
@@ -41,7 +44,6 @@ const createMap = (row, cell, mine) => {
     for(let j=0; j<mine; j++) {
         shuffle.push(table.splice(Math.floor(Math.random()*table.length), 1)[0]);
     }
-    console.log(shuffle + ' 마인의 인덱스');
     table = [];
     for(let k=0; k<row*cell; k++) {
         if(shuffle.includes(k)){
@@ -50,6 +52,8 @@ const createMap = (row, cell, mine) => {
         }
         table.push(CODE.NORMAL);
     }
+    console.log(table + ' 현재의 테이블');
+    return table;
 }
 
 function reducer(state, action) {
@@ -65,6 +69,63 @@ function reducer(state, action) {
                 tableData: createMap(action.row, action.cell, action.mine),
                 stop: false,
             }
+        case OPEN_BOX:
+            let newMap = [...state.tableData];
+            let around = [];
+            let option = action.opendCell;
+            let top = option >= state.data.cell ? true : false;
+            let bottom = (state.data.row - 1) * state.data.cell > option ? true : false;
+            let left = option%state.data.cell !== 0;
+            let right = (option+1)%state.data.cell !== 0;
+            if(left) {
+                console.log('왼쪽: ' + left);
+                around = around.concat(newMap[action.opendCell-1]);
+                if(top) {// 왼쪽이 있고 위도 있는 경우
+                    console.log('왼쪽: ' + left + ' 위: ' + top);
+                    around = around.concat(
+                        newMap[action.opendCell - state.data.cell - 1],
+                        );
+                }
+                if(bottom) { // 왼쪽이 있고 아래도 있는 경우
+                    console.log('왼쪽: ' + left + ' 아래: ' + bottom);
+                    around = around.concat(
+                        newMap[action.opendCell + (state.data.cell - 1)],
+                        );
+                }
+            }
+            if(right) {
+                console.log('오른쪽: ' + right);
+                around = around.concat(newMap[action.opendCell+1]);
+                if(top) {// 오른쪽이 있고 위도 있는 경우
+                    console.log('오른쪽: ' + right + ' 위: ' + top);
+                    around = around.concat(
+                        newMap[action.opendCell - state.data.cell + 1],
+                        );
+                }
+                if(bottom) { // 오른쪽이 있고 아래도 있는 경우
+                    console.log('오른쪽: ' + right + ' 아래: ' + bottom);
+                    around = around.concat(
+                        newMap[action.opendCell + state.data.cell + 1],
+                        );
+                }
+            }
+            if(top) {
+                console.log('위: ' + top);
+                around = around.concat(newMap[action.opendCell - state.data.cell]);
+            }
+            if(bottom) {
+                console.log('아래: ' + bottom);
+                around = around.concat(newMap[action.opendCell + state.data.cell]);
+            }
+            let count = around.filter(v => {
+                return [CODE.MINE, CODE.QUESTION_MINE, CODE.FLAG_MINE].includes(v)}
+                ).length;
+            console.log(around);
+            newMap[action.opendCell] = count;
+            return {
+                ...state,
+                tableData: newMap,
+            }
         default:
             return state;
     }
@@ -74,10 +135,13 @@ const App = (props) => {
     
     const [state, dispatch] = useReducer(reducer, initialState);
     const {stop, timer, tableData} = state;
-    const value = useMemo(() => ({dispatch}), []);
+    const value = useMemo(() => ({dispatch, tableData, state}), [tableData]);
     return (
         <MyContext.Provider value={value}>
-            <Form/>
+            <div className="App-container">
+                <Form/> 
+                <Table/>
+            </div>
         </MyContext.Provider>
     )
 }
